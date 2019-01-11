7495065d0e5662920ee0fd99e63d1c63c5cf402c
Your Name <you@example.com>
2019-01-11 16:17:24 +0100

    0.2.0
    =0.2.0
    
    Set theme jekyll-theme-midnight
    0.2.0 - Added licenece

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -1,10 +1,9 @@
 data:extend({
 
- {
-	
+  {
     type = "item",
     name = "speed-train",
-    icon = "__SO-Speed-Train__/graphics/icons/speed-train.png",
+    icon = "__OS-Speed-Train__/graphics/icons/speed-train.png",
     icon_size = 32,
     flags = {"goes-to-quickbar"},
     subgroup = "transport",
@@ -12,6 +11,21 @@ data:extend({
     place_result = "speed-train",
     stack_size = 5
 	
-  }
+  },
+  {
+    type = "item",
+    name = "speed-train-fuel",
+    icon = "__OS-Speed-Train__/graphics/icons/speed-train-fuel.png",
+    icon_size = 32,
+    flags = {"goes-to-main-inventory"},
+    fuel_category = "chemical",
+    fuel_value = "0.86GJ",
+    fuel_acceleration_multiplier = 0.9,
+    fuel_top_speed_multiplier = 1.5,
+    fuel_glow_color = {r = 0.5, g = 0, b = 1},
+    subgroup = "intermediate-product",
+    order = "p[speed-train-fuel]",
+    stack_size = 5
+  },
 
 })
--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -2,15 +2,26 @@ data:extend({
  {
     type = "recipe",
     name = "speed-train",
-    enabled = "true",
+    enabled = "false",
     ingredients = 
     {
       {"locomotive",1},
       {"electronic-circuit",200},
+      {"advanced-circuit",50},
       {"iron-gear-wheel",50},
-      {"iron-plate",500},
-      {"steel-plate",50}
+      {"plastic-bar",150}
     },
     result = "speed-train"
+  },
+  {
+    type = "recipe",
+    name = "speed-train-fuel",
+    enabled = "false",
+    ingredients = 
+    {
+      {"rocket-fuel",2},
+      {"sulfur",3}
+    },
+    result = "speed-train-fuel"
   }
 })
